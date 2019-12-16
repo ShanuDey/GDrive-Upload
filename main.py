18d95ab58a9478cecd1a502788331869dd4c87fa
ShanuDey <sd4shanudey@gmail.com>
2019-12-16 23:34:50 +0530

    Test file read
    Signed-off-by: ShanuDey <sd4shanudey@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from pydrive.auth import GoogleAuth
 from pydrive.drive import GoogleDrive
+import os
 
 # Login
 gLogin = GoogleAuth()
 gLogin.LocalWebserverAuth()
 drive = GoogleDrive(gLogin)
+
+
+with open("test.txt","r") as file:
+	print(file.read())
